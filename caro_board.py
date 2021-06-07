72cf9ed778c0a9d7c96f0264a7afe4406492c285
hgqbinh2002@gmail.com <hgqbinh2002@gmail.com>
2021-06-07 16:38:15 +0700

    Replace old class with new class (inherited from pygame.sprite.Sprite) Clean up dirty code in mainloop.py

--- caro_board.py
+++ caro_board.py
@@ -1,88 +1,196 @@
-class XPlayer:
-    mark = 'x'
-    def __init__(self, board):
-        self.board = board
-        self.row = len(self.board)
-        self.col = len(self.board[0])
-    def is_valid_move(self, *coordinate):
-        if (len(coordinate) == 1):
-            coordinate = coordinate[0]
-        try:
-            if self.board[coordinate[0]][coordinate[1]] == '.':
-                return 1
-        except:
-            pass
-        return 0
-    def play(self, *coordinate):
-        if (len(coordinate) == 1):
-            coordinate = coordinate[0]
-        if (self.is_valid_move(coordinate[0], coordinate[1])):
-            self.board[coordinate[0]][coordinate[1]] = self.mark
-            return 1
-        return 0    
-    def is_winning_vertically(self):
-        result = []
-        for j in range(self.col):
-            for i in range(self.row - 4):
-                if self.board[i][j] == self.mark:
-                    if self.board[i + 1][j] == self.mark:
-                        if self.board[i + 2][j] == self.mark:
-                            if self.board[i + 3][j] == self.mark:
-                                if self.board[i + 4][j] == self.mark:
-                                    return [[i, j], [i+1, j], [i+2, j], [i+3, j], [i+4, j]]
-        return []
-    def is_winning_horizontally(self):
-        for i in range(self.row):
-            for j in range(self.col - 4):
-                if self.board[i][j] == self.mark:
-                    if self.board[i][j + 1] == self.mark:
-                        if self.board[i][j + 2] == self.mark:
-                            if self.board[i][j + 3] == self.mark:
-                                if self.board[i][j + 4] == self.mark:
-                                    return [[i, j], [i, j+1], [i, j+2], [i, j+3], [i, j+4]]
-        return []
-    def is_winning_diagonally(self):
-        for i in range(self.row - 4):
-            for j in range(self.col - 4):
-                if self.board[i][j] == self.mark:
-                    if self.board[i + 1][j + 1] == self.mark:
-                        if self.board[i + 2][j + 2] == self.mark:
-                            if self.board[i + 3][j + 3] == self.mark:
-                                if self.board[i + 4][j + 4] == self.mark:
-                                    return [[i, j], [i+1, j+1], [i+2, j+2], [i+3, j+3], [i+4, j+4]]
-        return []
-    def is_winning(self):
-        if result := self.is_winning_vertically():
-            return result
-        if result := self.is_winning_horizontally():
-            return result
-        if result := self.is_winning_diagonally():
-            return result
-        return []
-        
+import pygame
 
-class OPlayer(XPlayer):
-    mark = 'o'
+color_palettes = {
+    "mysterious": ["#581b98", "#9c1de7", "#f3558e", "#faee1c", "#8cff75"],
+    "freezing": ['#7d8aff', '#6db3de', '#84f5e4', '#6dde8e', '#cdfab6']
+}
+color_palette = color_palettes["mysterious"]
+for i in  range(len(color_palette)):
+    color_palette[i] = pygame.Color(color_palette[i])
 
-        
+res = width, height = 720, 720
+pygame.init()
+pygame.display.init()
+screen = pygame.display.set_mode(res)
+screen.fill(color_palette[1])
+
 class Board:
+    __instance = None
     def __init__(self, row = 10, col = 10):
+        if Board.__instance != None:
+            raise Exception("Board is a singleton class")
+        Board.__instance = self
+        self.square_width = 40
+        self.x_offset = 100
+        self.y_offset = 100
         self.row = row
         self.col = col
         self.board = [['.' for j in range(self.col)] for i in range(self.row)]
-        self.xPlayer = XPlayer(self.board)
-        self.oPlayer = OPlayer(self.board)
-        self.log = []
-##    def __getitem__(self, row):
-##        return self.board[row]
+
+    def add_mark(self, x, y, mark):
+        self.board[x][y] = mark
+
+
+    def __getitem__(self, i):
+        return self.board[i]
+    def get_inst():
+        return Board.__instance
+
+
     def __str__(self):
         return '\n'.join([' '.join([str(cell) for cell in row]) for row in self.board])
+
+    
     def __repr__(self):
         return '\n'.join([' '.join([str(cell) for cell in row]) for row in self.board])
 
     
-##board = Board()
-##print(board)
-##print()
-##board.xPlayer.play((3, 4))
-##print(board)
+    def draw(self):
+        for i in range(self.row):
+            for j in range(self.col):
+                rect = pygame.Rect(i * self.square_width + self.x_offset, j * self.square_width + self.y_offset, self.square_width, self.square_width)
+                pygame.draw.rect(screen, color_palette[2], rect, 1, 2)
+    
+
+class MoveLog():
+    def __init__(self):
+        self.moveLog = pygame.sprite.Group()
+
+        
+    def draw(self):
+        self.moveLog.draw(screen)
+
+        
+    def add(self, markObject):
+        self.moveLog.add(markObject)
+
+    def __len__(self):
+        return len(self.moveLog)
+        
+
+class Mark(pygame.sprite.Sprite):
+    def __init__(self, x, y, mark):
+        super().__init__()
+        self.mark = mark
+        if (self.mark == 'x'):
+            corner_offset = Board.get_inst().square_width // 7
+            self.color = color_palette[0]
+            self.image = pygame.Surface((Board.get_inst().square_width, Board.get_inst().square_width))
+            pygame.draw.line(self.image, self.color, (corner_offset, corner_offset),
+                             (Board.get_inst().square_width - corner_offset, Board.get_inst().square_width - corner_offset), 5)
+            pygame.draw.line(self.image, self.color, (Board.get_inst().square_width - corner_offset, corner_offset),
+                             (corner_offset, Board.get_inst().square_width - corner_offset), 5)
+        else:
+            self.image = pygame.Surface((Board.get_inst().square_width, Board.get_inst().square_width))
+            pygame.draw.circle(self.image, color_palette[2], (Board.get_inst().square_width // 2, Board.get_inst().square_width // 2), Board.get_inst().square_width // 2, 5)
+        self.image = pygame.transform.scale(self.image, (Board.get_inst().square_width, Board.get_inst().square_width))
+        self.image.set_colorkey((0,0,0))
+        self.rect = self.image.get_rect()
+        self.x = (x - Board.get_inst().x_offset) // Board.get_inst().square_width
+        self.y = (y - Board.get_inst().y_offset) // Board.get_inst().square_width
+        self.rect.x = self.x * Board.get_inst().square_width + Board.get_inst().x_offset
+        self.rect.y = self.y * Board.get_inst().square_width + Board.get_inst().y_offset    
+
+
+class Caro:
+    def __init__(self, board_row = 10, board_col = 10):
+        self.board = Board(board_row, board_col)
+        self.moveLog = MoveLog()
+        self.consecutive_num = 5
+        
+    def add_move(self, x, y):
+        board_y = (x - self.board.x_offset) // self.board.square_width
+        if 0 <= board_y < self.board.row:
+            board_x = (y - self.board.y_offset) // self.board.square_width
+            if 0 <= board_x < self.board.col:
+                if self.board[board_x][board_y] == '.':
+                    mark = None
+                    if len(self.moveLog) % 2 == 0:
+                        mark = 'x'
+                    else:
+                        mark = 'o'
+                    self.board[board_x][board_y] = mark
+                    markObject = Mark(x, y, mark)
+                    self.moveLog.add(markObject)
+
+        
+    def draw_move(self):
+        self.moveLog.draw()
+
+        
+    def draw_board(self):
+        self.board.draw()
+
+        
+    def draw(self):
+        screen.fill(color_palette[1])
+        self.draw_move()
+        self.draw_board()
+        mark = None
+        if len(self.moveLog) % 2 == 0:
+            mark = 'o'
+        else:
+            mark = 'x'
+        if winning_board_points := self.find_consecutive_marks(mark):
+            self.draw_winning_line(winning_board_points)
+
+        
+    def find_vertical_consecutive_marks(self, mark):
+        result = []
+        for j in range(self.board.col):
+            for i in range(self.board.row - 4):
+                for k in range(self.consecutive_num):
+                    if (self.board[i+k][j] == mark):
+                        result.append([i+k,j])
+                    else:
+                        result = []
+                        break
+                if len(result) == self.consecutive_num:
+                    return result
+        return result
+
+    
+    def find_horizontal_consecutive_marks(self, mark):
+        for i in range(self.board.row):
+            for j in range(self.board.col - 4):
+                for k in range(self.consecutive_num):
+                    if (self.board[i][j+k] == mark):
+                        result.append([i, j+k])
+                    else:
+                        result = []
+                        break
+                if len(result) == self.consecutive_num:
+                    return result
+        return result
+
+    
+    def find_diagonal_consecutive_marks(self, mark):
+        for i in range(self.board.row - 4):
+            for j in range(self.board.col - 4):
+                for k in range(self.consecutive_num):
+                    if (self.board[i+k][j+k] == mark):
+                        result.append([i+k, j+k])
+                    else:
+                        result = []
+                        break
+                if len(result) == self.consecutive_num:
+                    return result
+        return result
+
+    
+    def find_consecutive_marks(self, mark):
+        if result := self.find_vertical_consecutive_marks(mark):
+            return result
+        if result := self.find_horizontal_consecutive_marks(mark):
+            return result
+        if result := self.find_diagonal_consecutive_marks(mark):
+            return result
+        return []
+    def draw_winning_line(self, winning_board_points):
+        if len(winning_board_points) == self.consecutive_num:
+            pygame.draw.line(screen, color_palette[4],
+                             (winning_board_points[0][1] * self.board.square_width + self.board.x_offset + self.board.square_width // 2,
+                              winning_board_points[0][0] * self.board.square_width + self.board.y_offset + self.board.square_width // 2),
+                             (winning_board_points[self.consecutive_num - 1][1] * self.board.square_width + self.board.x_offset + self.board.square_width // 2,
+                              winning_board_points[self.consecutive_num - 1][0] * self.board.square_width + self.board.y_offset + self.board.square_width // 2), 5)
+            
--- mainloop.py
+++ mainloop.py
@@ -1,37 +1,12 @@
 import pygame, sys
-##import pygame_menu 
 import caro_board
 
-
-res = width, height = 720, 720
 fps_limit = 10
-color_palettes = {
-    "mysterious": ["#581b98", "#9c1de7", "#f3558e", "#faee1c", "#8cff75"],
-    "freezing": ['#7d8aff', '#6db3de', '#84f5e4', '#6dde8e', '#cdfab6']
-}
-color_palette = color_palettes["mysterious"]
-for i in  range(len(color_palette)):
-    color_palette[i] = pygame.Color(color_palette[i])
-
-pygame.init()
-pygame.display.init()
-screen = pygame.display.set_mode(res)
-screen.fill(color_palette[1])
 clock = pygame.time.Clock()
 
 done = False
-ite = 0
-board_row = 10
-board_col = 10
-square_width = 40
-square_offset = 100
-board = caro_board.Board(board_row, board_col)
-
-
-for i in range(board_row):
-    for j in range(board_col):
-        rect = pygame.Rect(i * square_width + square_offset, j * square_width + square_offset, square_width, square_width)
-        pygame.draw.rect(screen, color_palette[2], rect, 1, 2) 
+caro = caro_board.Caro()
+caro.draw_board()
 
 while not done:
     clock.tick(fps_limit)
@@ -40,39 +15,11 @@ while not done:
             done = True
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = list(event.pos)
-            board_pos = [0, 0]
-            for i in range(2):
-                board_pos[i] = (pos[i] - square_offset) // square_width
-                pos[i] = board_pos[i] * square_width + square_offset # Position readjustment for X and O mark
-            
-            if ite % 2 == 0:
-                if board.xPlayer.play([board_pos[0], board_pos[1]]):
-                    pygame.draw.line(screen, color_palette[3], pos, (pos[0] + square_width, pos[1] + square_width), 5)
-                    pygame.draw.line(screen, color_palette[3], (pos[0] + square_width, pos[1]), (pos[0], pos[1] + square_width), 5)
-                    consecutive_points = board.xPlayer.is_winning()
-                    if len(consecutive_points) == 5:
-                        for i in range(2):            
-                            consecutive_points[0][i] = consecutive_points[0][i] * square_width + square_offset + square_width / 2
-                            consecutive_points[4][i] = consecutive_points[4][i] * square_width + square_offset + square_width / 2
-                        pygame.draw.line(screen, color_palette[4], consecutive_points[0], consecutive_points[4], 20)
-            
-                    ite += 1
-            else:
-                if board.oPlayer.play([board_pos[0], board_pos[1]]):
-                    rect = pygame.Rect(pos, (square_width, square_width))
-                    pygame.draw.ellipse(screen, color_palette[0], rect, 5)
-                    consecutive_points = board.oPlayer.is_winning()
-                    if len(consecutive_points) == 5:
-                        for i in range(2):            
-                            consecutive_points[0][i] = consecutive_points[0][i] * square_width + square_offset + square_width / 2
-                            consecutive_points[4][i] = consecutive_points[4][i] * square_width + square_offset + square_width / 2
-                        pygame.draw.line(screen, color_palette[4], consecutive_points[0], consecutive_points[4], 20)
-            
-                    ite += 1
-            print(board)
-            
-            
+            caro.add_move(pos[0], pos[1])
+            print(caro.board)
+            caro.draw()
 
+    
     pygame.display.flip()
 
 pygame.quit()
